686bbee3fe89d2073617517c1b8a6d11f93ff816
Jared Orihuela Contreras <112034917+JaocHatter@users.noreply.github.com>
2024-06-25 19:43:07 -0500

    Delete src/ProductoRepository.java

--- src/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Producto;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductoRepository extends CrudRepository<Producto,String> {
-}
